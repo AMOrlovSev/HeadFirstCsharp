6ae5c7fabd9b07045d38e3c9cba7ca69dbbb9574
Artem Orlov <amorlov.sev@gmail.com>
2024-05-01 22:51:00 +0300

    Добавьте файлы проекта.

---
+++ 002_MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _002_MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "🐱‍👤","🐱‍👤",
+                "🐱‍🏍","🐱‍🏍",
+                "🙈","🙈",
+                "🦊","🦊",
+                "🦝","🦝",
+                "🐷","🐷",
+                "🐲","🐲",
+                "🐸","🐸",
+            };
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
