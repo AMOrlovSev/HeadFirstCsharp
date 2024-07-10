58e02b9f5f7b0a54136544d0760a548c3de8bb24
Artem Orlov <amorlov.sev@gmail.com>
2024-07-10 22:02:57 +0300

    09_chapter9

---
+++ 09_chapter9/01_LinqTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_LinqTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 1; i <= 99; i++)
+                numbers.Add(i);
+            IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5));
+            foreach (int i in firstAndLastFive)
+            {
+                Console.Write($"{i} ");
+            }
+
+            foreach (int i in Enumerable.Range(5, 100))
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
