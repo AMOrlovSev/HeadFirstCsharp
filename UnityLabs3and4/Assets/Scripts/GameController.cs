b5a38bd8cdcef0d1f7dbe1c3ca6f42ee29cad3e6
Artem Orlov <amorlov.sev@gmail.com>
2024-07-10 14:30:59 +0300

    unity4

--- UnityLabs3and4/Assets/Scripts/GameController.cs
+++ UnityLabs3and4/Assets/Scripts/GameController.cs
@@ -1,16 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameController : MonoBehaviour
 {
+
+    public Button PlayAgainButton;
+    public TextMeshProUGUI ScoreText;
+
     public GameObject OneBallPrefab;
+    public int Score = 0;
+    public bool GameOver = true;
+    public int NumberOfBalls = 0;
+    public int MaximumBalls = 15;
     void Start()
     {
         InvokeRepeating("AddABall", 1.5F, 1);
     }
     void AddABall()
     {
-        Instantiate(OneBallPrefab);
+        if (!GameOver) 
+        {
+            Instantiate(OneBallPrefab);
+            NumberOfBalls++;
+            if (NumberOfBalls > MaximumBalls)
+            {
+                GameOver = true;
+                PlayAgainButton.gameObject.SetActive(true);
+            }
+        }
+    }
+    public void ClickedOnBall()
+    {
+        Score++;
+        NumberOfBalls--;
+    }
+
+    public void StartGame()
+    {
+        foreach (GameObject ball in GameObject.FindGameObjectsWithTag("GameController"))
+        {
+            Destroy(ball);
+        }
+        PlayAgainButton.gameObject.SetActive(false);
+        Score = 0;
+        NumberOfBalls = 0;
+        GameOver = false;
+    }
+    void Update()
+    {
+        ScoreText.text = Score.ToString();
     }
 }
--- UnityLabs3and4/Assets/Scripts/OneBallBehaviour.cs
+++ UnityLabs3and4/Assets/Scripts/OneBallBehaviour.cs
@@ -4,24 +4,64 @@ using UnityEngine;
 
 public class OneBallBehaviour : MonoBehaviour
 {
-    public float XRotation = 0;
-    public float YRotation = 1;
-    public float ZRotation = 0;
-    public float DegreesPerSecond = 180;
+    public float TooFar = 5;
+
+    //public float XRotation = 0;
+    //public float YRotation = 1;
+    //public float ZRotation = 0;
+    public float XSpeed;
+    public float YSpeed;
+    public float ZSpeed;
+
+    //public float DegreesPerSecond = 180;
+    public float Multiplier = 0.75f;
+
     public int ballNumber = 0;
     static int counter;
     // Start вызывается перед первым обновлением кадра
     void Start()
     {
-        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
+        //transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
         counter++;
         ballNumber = counter;
+
+        ResetBall();
     }
     // Update вызывается один раз на кадр
     void Update()
     {
-        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
-        transform.RotateAround(Vector3.zero, axis, DegreesPerSecond * Time.deltaTime);
-        // Debug.DrawRay(Vector3.zero, axis, Color.yellow);
+        //Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        //transform.RotateAround(Vector3.zero, axis, DegreesPerSecond * Time.deltaTime);
+        //// Debug.DrawRay(Vector3.zero, axis, Color.yellow);
+
+        XSpeed += Multiplier - Random.value * Multiplier * 2;
+        YSpeed += Multiplier - Random.value * Multiplier * 2;
+        ZSpeed += Multiplier - Random.value * Multiplier * 2;
+
+        transform.Translate(Time.deltaTime * XSpeed, Time.deltaTime * YSpeed, Time.deltaTime * ZSpeed);
+
+        if ((Mathf.Abs(transform.position.x) > TooFar)
+            || (Mathf.Abs(transform.position.y) > TooFar)
+            || (Mathf.Abs(transform.position.z) > TooFar))
+        {
+            ResetBall();
+        }
+    }
+    void OnMouseDown()
+    {
+        GameController controller = Camera.main.GetComponent<GameController>();
+        if (!controller.GameOver)
+        {
+            controller.ClickedOnBall();
+            Destroy(gameObject);  
+        }
+    }
+
+    void ResetBall()
+    {
+        XSpeed = Multiplier - Random.value * Multiplier * 2;
+        YSpeed = Multiplier - Random.value * Multiplier * 2;
+        ZSpeed = Multiplier - Random.value * Multiplier * 2;
+        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
     }
 }
