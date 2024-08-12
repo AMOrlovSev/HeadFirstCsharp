037126af5347006d405677c89ba5758aa0ad282a
Artem Orlov <amorlov.sev@gmail.com>
2024-08-12 13:35:12 +0300

    12_chapter12

---
+++ 12_chapter12/Ex03_Pool/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.IO;
+
+namespace Ex03_Pool
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kangaroo joey = new Kangaroo();
+            int koala = joey.Wombat(
+            joey.Wombat(joey.Wombat(1)));
+            try
+            {
+                Console.WriteLine((15 / koala)
+                + " eggs per pound");
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("G'Day Mate!");
+            }
+        }
+    }
+
+    class Kangaroo
+    {
+        FileStream fs;
+        int croc;
+        int dingo = 0;
+        public int Wombat(int wallaby)
+        {
+            dingo++;
+            try
+            {
+                if (wallaby > 0)
+                {
+                    fs = File.OpenWrite("wobbiegong");
+                    croc = 0;
+                }
+                else if (wallaby < 0)
+                {
+                    croc = 3;
+                }
+                else
+                {
+                    fs = File.OpenRead("wobbiegong");
+                    croc = 1;
+                }
+            }
+            catch (IOException)
+            {
+                croc = -3;
+            }
+            catch
+            {
+                croc = 4;
+            }
+            finally
+            {
+                if (dingo > 2)
+                {
+                    croc -= dingo;
+                }
+            }
+            return croc;
+        }
+    }
+}
---
+++ 12_chapter12/Ex04_Magnets/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Ex04_Magnets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("when it ");
+            ExTestDrive.Zero("yes");
+            Console.Write(" it ");
+            ExTestDrive.Zero("no");
+            Console.WriteLine(".");
+        }
+    }
+    class MyException : Exception { }
+    class ExTestDrive
+    {
+        public static void Zero(string test)
+        {
+            try
+            {
+                Console.Write("t");
+                DoRisky(test);
+                Console.Write("o");
+            }
+            catch (MyException)
+            {
+                Console.Write("a");
+            }
+            finally
+            {
+                Console.Write("w");
+            }
+            Console.Write("s");
+        }
+        static void DoRisky(String t)
+        {
+            Console.Write("h");
+            if (t == "yes")
+            {
+                throw new MyException();
+            }
+            Console.Write("r");
+        }
+    }
+}
